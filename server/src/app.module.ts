d6bfc1b1419ca5074ac8af047fd09e65ec9f1a4b
Nasreddine Bac Ali <nasreddine.bacali95@gmail.com>
2020-12-13 04:08:54 +0100

    Add TypeORM to server

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { entities } from './entities';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOSTNAME,
+      port: parseInt(process.env.DATABASE_PORT),
+      database: process.env.DATABASE_NAME,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      entities: entities,
+    }),
+    TypeOrmModule.forFeature(entities),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
