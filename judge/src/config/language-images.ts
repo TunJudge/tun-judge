f86c473ab9dc92a7b9c064d2fc5829a89367070e
Nasreddine Bac Ali <nasreddine.bacali95@gmail.com>
2021-01-05 17:21:28 +0100

    Edit images tags

--- judge/src/config/language-images.ts
+++ judge/src/config/language-images.ts
@@ -1,8 +1,8 @@
 const languageImages = {
-  c: 'tun-judge/gcc:7.3.0',
-  'c++': 'tun-judge/gcc:7.3.0',
-  java: 'tun-judge/openjdk:8-jre-alpine',
-  javascript: 'tun-judge/node:14.15.1-alpine',
+  c: 'tunjudge/lang-cpp:7.3.0',
+  'c++': 'tunjudge/lang-cpp:7.3.0',
+  java: 'tunjudge/lang-java:8-jre-alpine',
+  javascript: 'tunjudge/lang-node:14.15.1-alpine',
 };
 
 export type Language = keyof typeof languageImages;
