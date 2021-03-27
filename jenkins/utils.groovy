3725e03f2b52c5597211c2fcf49e02adcde0281a
Nasreddine Bac Ali <nasreddine.bacali95@gmail.com>
2021-03-27 19:23:10 +0100

    Update Jenkinsfile.release

--- jenkins/utils.groovy
+++ jenkins/utils.groovy
@@ -1,5 +1,5 @@
 def runInDocker(imageTag, command) {
-    sh "docker run --rm -v ${env.WORKSPACE}:/app -w /app ${imageTag} ${command}"
+    sh "docker run --rm -v '${env.WORKSPACE}:/app' -w /app ${imageTag} ${command}"
 }
 
 return this
