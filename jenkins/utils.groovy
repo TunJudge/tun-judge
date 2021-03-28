717636f5e04b3abd7897354e0c900b5e61f8550d
Nasreddine Bac Ali <nasreddine.bacali95@gmail.com>
2021-03-28 15:38:28 +0200

    ci: add release stage to Jenkinsfile.release

--- jenkins/utils.groovy
+++ jenkins/utils.groovy
@@ -1,5 +1,17 @@
 def runInDocker(imageTag, command) {
-    sh "docker run --rm -v '${env.WORKSPACE}:/app' -w /app ${imageTag} ${command}"
+    return sh "docker run --rm -v '${env.WORKSPACE}:/app' -w /app ${imageTag} ${command}"
+}
+
+def getReleaseTags(imageTag, isLatest = false) {
+    def currentVersion = runInDocker(imageTag, "yarn version:current")
+    def versionParts = currentVersion.split(".")
+
+    def tags = isLatest ? ["latest"] : []
+    tags << versionParts[0]
+    tags << versionParts[0] + "." + versionParts[1]
+    tags << currentVersion
+
+    return tags
 }
 
 return this
