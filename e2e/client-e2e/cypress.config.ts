5c4f84c012716379beb50394d5a83abfd226425f
Nasreddine Bac Ali <nasreddine.bacali95@gmail.com>
2024-09-11 22:34:55 +0000

    chore: add client project

---
+++ e2e/client-e2e/cypress.config.ts
@@ -0,0 +1,19 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      bundler: 'vite',
+      webServerCommands: {
+        default: 'yarn nx run client:serve',
+        production: 'yarn nx run client:preview',
+      },
+      ciWebServerCommand: 'yarn nx run client:preview',
+      ciBaseUrl: 'http://localhost:4300',
+    }),
+    baseUrl: 'http://localhost:4200',
+  },
+});
