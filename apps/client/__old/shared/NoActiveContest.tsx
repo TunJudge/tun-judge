b24c8ffa3b0d69accdd05768de6f90fc80c3b2cc
Nasreddine Bac Ali <nasreddine.bacali95@gmail.com>
2024-11-01 06:21:53 +0000

    chore: remove old files

--- apps/client/__old/shared/NoActiveContest.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const NoActiveContest: React.FC = () => (
-  <div className="flex h-full w-full items-center justify-center py-4 text-3xl font-medium text-gray-400">
-    No Active Contest
-  </div>
-);
--- apps/client/__old/stores/useStore.ts
+++
@@ -1,8 +0,0 @@
-import { MobXProviderContext } from 'mobx-react';
-import { useContext } from 'react';
-
-import { Stores } from '.';
-
-export function useStore<T>(name: keyof Stores): T {
-  return useContext(MobXProviderContext)[name] as T;
-}
